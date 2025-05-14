c08bedf6fd0d1c603ea98fb3ea3ea9708639cfe5
Jon 赚大钱 <janders@radix.trade>
2025-05-14 18:08:04 -0500

    Initial Commit

---
+++ Hobbs Time Extractor/Hobbs_Time_ExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hobbs_Time_ExtractorApp.swift
+//  Hobbs Time Extractor
+//
+//  Created by Jonathan Anderson on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Hobbs_Time_ExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
