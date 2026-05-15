51955027cb031bc1423ac2bfee1cc8a6a021361a
Jon 赚大钱 <jonathan.anderson@gmail.com>
2026-05-15 01:19:36 -0500

    updated

--- Hobbs Time Extractor/Hobbs_Time_ExtractorApp.swift
+++ Hobbs Time Extractor/Hobbs_Time_ExtractorApp.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 #if canImport(UIKit)
 import UIKit
 
 >>>>>>> 137c03c (updated)
+=======
+#if canImport(UIKit)
+import UIKit
+
+>>>>>>> 6bb4cc9 (updated)
 // Required on iPhone (iOS 16+) for portrait-upside-down to be honoured at runtime.
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
@@ -23,10 +29,16 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+#endif
+>>>>>>> 6bb4cc9 (updated)
 
 @main
 struct Hobbs_Time_ExtractorApp: App {
+    #if canImport(UIKit)
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+<<<<<<< HEAD
 =======
 #endif
 
@@ -36,6 +48,9 @@ struct Hobbs_Time_ExtractorApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     #endif
 >>>>>>> 137c03c (updated)
+=======
+    #endif
+>>>>>>> 6bb4cc9 (updated)
 
     var body: some Scene {
         WindowGroup {
